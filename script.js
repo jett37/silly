6e3b8b1caeeb951e4c5c36b2eab288acda20c4ac
jett37 <160654647+jett37@users.noreply.github.com>
2024-02-21 09:56:52 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+.container * {
+  border: 1px solid blue;
+  position: relative;
+}
+
+.container *:after {
+  content:attr(class);
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: grid;
+  display: inline block;
+}
+#0a0a23
+  "aboutme dd-link1 dd-link2 dd-link3 art-blog-link contact-link"
+font-family: "Spinstee", sans-serif;
+#8F7
+
+ background-image: url("https://lh3.googleusercontent.com/pw/ABLVV852IuFbZ6Oa7dstG6wnz0K53c5N3iQ7Z9jr6gXEmcMgrBvG0tDJw4lAM-MEv4DsXEmt2bwMzL-un82-TL3n0j61if1RmLsrOGSLSGkF6OOh6Gmf5XEw8Ym5GdwJjVjd3qs_qfa80fAw42BWzcns-rSSKgOQnZ8SFotBc07oolHI6wG_M3CJ0xS1FMaFiGIuVWO4LQe3PJ5HuIry4i_LUkA6AP6-WwSLvCv97t0uB5vuQvLnae6WIg6xeO8SQnJQ6OorWOfTLOkt3KBqf1coUmasAZWPYs7dgwbFotvabXgzT3YUBE-N-kz1zTPCl__2FUt81idt6ofg8TTbHkkSwMZCCDocDd-6dGcYe8LpJrINd7raHlTSgx3SAL3TWoHpUCA6lmaDq2ayXaW-fNocf9DVLHLq_jE4MQg6Av-hLj3VxAbq4Dl7y-9SRzjj0osvB1LIls5z3s51lK-XQn3nlsZherSmeTYv6ajAt7c7nJo-wJSsV7uz6BbKbFzsmHnYzCW4fdKRja-qvYwAx7Iw1XOYypKDsKYrEpIFLa7Qohpbu96M9y7RSHP-uHSzsBbhkIK2zuuzQguGgGyMNOT23y4r56BXqoXTy_UlWrtnFZULr-CQDB3SwTkX484nRyIMTH1uoF_y4pHScxdsFYHeRMX7A89KUP5qWGzFw5XISQM3Y6TLWRC0jte0GnfYLyl0ya6BM_Jxek231pmu5EUwb2bNlr0N5EFgrrmvKwK7rMgI0zfaJmy37O0pMR4B-IZkyjI-fel4H3zJ_gLmMDN_J9F0_Hnfr0DIPfzwDtjbtwAMc3hSFpDFT8xcV1osm7iKQqlKgAtgJXgssk0wvHoeTU-xOE3hYb1-J01O5xwqazfmrwRrk3GomY1Ym0rPRnLyQHht4989oOsPRGACfcEnuRtWEQ=w1345-h686-s-no-gm?authuser=0");
+  background-position: center; /* Center the image */
+  background-repeat: no-repeat; /* Do not repeat the image */
+  background-size: cover;
+  background-clip: text;
+-webkit-background-clip: text;
+color: transparent;
